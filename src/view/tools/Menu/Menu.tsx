4ba32177db5cddb5c9a9bbc7ddf6c473b0576efb
cuiyue <cuiyue.msn@hotmail.com>
2020-01-21 10:38:39 +0800

    修改为函数组件

--- src/view/tools/Menu/Menu.tsx
+++ src/view/tools/Menu/Menu.tsx
@@ -1,46 +1,50 @@
 import path from 'path';
 import { execFile } from 'child_process';
 import { ipcRenderer, IpcRendererEvent } from 'electron';
-import React, { Component, PropsWithChildren, useEffect, useState, MouseEvent } from 'react';
+import React, { FC, useEffect, useState, MouseEvent } from 'react';
 import Rpc from '@src/service/rpc';
-// import { Link } from 'dva/router';
-import debounce from 'lodash/debounce';
-import Modal from 'antd/lib/modal';
+import config from '@src/config/ui.config.json';
+import Modal from 'antd/lib/Modal';
 import Spin from 'antd/lib/spin';
+import message from 'antd/lib/message';
 import ImportDataModal from './components/ImportDataModal/ImportDataModal';
 import CImportDataInfo from '@src/schema/CFetchDataInfo';
-import config from '@src/config/ui.config.json';
 import './Menu.less';
-import { message } from 'antd';
 
-interface IProp { }
-interface IState {
-    isLoading: boolean;
-    importDataModalVisible: boolean;
-}
+interface Prop { }
 
-class Menu extends Component<IProp, IState>{
-    constructor(props: IProp) {
-        super(props);
-        this.state = {
-            isLoading: false,
-            importDataModalVisible: false
+/**
+ * 工具箱菜单
+ * @param props 属性
+ */
+const Menu: FC<Prop> = (props) => {
+
+    const [isLoading, setLoading] = useState<boolean>(false);
+    const [isOpenExe, setOpenExe] = useState<boolean>(false);
+    const [importDataModalVisible, setImportDataModalVisible] = useState<boolean>(false);
+
+    useEffect(() => {
+        ipcRenderer.on('receive-publish-path', receivePublishPathHandle);
+        return function () {
+            ipcRenderer.removeListener('receive-publish-path', receivePublishPathHandle);
         }
-    }
-    componentDidMount() {
-        ipcRenderer.on('receive-publish-path', this.receivePublishPathHandle);
-    }
-    componentWillUnmount() {
-        ipcRenderer.removeListener('receive-publish-path', this.receivePublishPathHandle)
-    }
-    receivePublishPathHandle = (event: IpcRendererEvent, args: string) => {
+    }, []);
+
+    /**
+     * 取得发布目录
+     * @param event IPC事件对象
+     * @param args 发布路径(*.asar文件)
+     */
+    const receivePublishPathHandle = (event: IpcRendererEvent, args: string) => {
         const { defenderPath } = config as any;
-        this.runExe(path.resolve(args, '../../../', defenderPath));
-    }
-    buttonClick = (e: MouseEvent<HTMLAnchorElement>) => {
-        ipcRenderer.send('publish-path');
-    }
-    runExe = (exePath: string) => {
+        runExe(path.resolve(args, '../../../', defenderPath));
+    };
+    /**
+     * 运行exe文件
+     * @param exePath 绝对路径
+     */
+    const runExe = (exePath: string) => {
+        setOpenExe(true);
         execFile(exePath, {
             windowsHide: false
         }, (err: Error | null, stdout: string | Buffer, stderr: string | Buffer) => {
@@ -51,81 +55,97 @@ class Menu extends Component<IProp, IState>{
                     content: '口令工具启动失败'
                 });
             }
+            setOpenExe(false);
         });
     }
-    importDataModalSaveHandle = (data: CImportDataInfo) => {
+
+    /**
+     * 口令工具Click
+     * @param e 事件对象
+     */
+    const passwordToolsClick = (e: MouseEvent<HTMLAnchorElement>) => {
+        ipcRenderer.send('publish-path');
+    }
+
+    /**
+     * 导入第三方数据回调
+     * @param data CImportDataInfo数据
+     */
+    const importDataModalSaveHandle = (data: CImportDataInfo) => {
         const rpc = new Rpc();
-        this.setState({ isLoading: true });
+        setLoading(true);
         rpc.invoke('ImportThirdData', [data]).then(() => {
             message.success('导入成功');
-            this.setState({ importDataModalVisible: false });
+            setImportDataModalVisible(false);
         }).catch((err) => {
             message.error('导入失败');
         }).finally(() => {
-            this.setState({ isLoading: false });
+            setLoading(false);
         });
     }
-    importDataModalCancelHandle = () => {
-        this.setState({ importDataModalVisible: false });
+    /**
+     * 关闭导入弹框
+     */
+    const importDataModalCancelHandle = () => {
+        setImportDataModalVisible(false);
     }
-    render() {
-        return <div className="tools-menu">
-            <menu>
-                <li>
-                    <Spin tip="正在打口令工具, 请稍候..." spinning={this.state.isLoading}>
-                        <a onClick={this.buttonClick}>
-                            <i className="lock"></i>
-                            <div className="info">
-                                <span>口令工具</span>
-                                <em>获取锁屏密码</em>
-                            </div>
-                        </a>
-                    </Spin>
-                </li>
-                <li>
-                    <a onClick={() => Modal.info({ title: 'BCP生成', content: '新功能，敬请期待', okText: '确定' })}>
-                        <i className="bcp"></i>
+
+    return <div className="tools-menu">
+        <menu>
+            <li>
+                <Spin tip="正在打口令工具, 请稍候..." spinning={isOpenExe}>
+                    <a onClick={passwordToolsClick}>
+                        <i className="lock"></i>
                         <div className="info">
-                            <span>BCP生成</span>
-                            <em>将报告文件生成BCP文件</em>
+                            <span>口令工具</span>
+                            <em>获取锁屏密码</em>
                         </div>
                     </a>
-                </li>
-                <li>
-                    <a onClick={() => Modal.info({ title: 'BCP上传', content: '新功能，敬请期待', okText: '确定' })}>
-                        <i className="upload"></i>
-                        <div className="info">
-                            <span>BCP上传</span>
-                            <em>将案件上传到指定FTP服务器</em>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a onClick={() => Modal.info({ title: '报告生成', content: '新功能，敬请期待', okText: '确定' })}>
-                        <i className="report"></i>
-                        <div className="info">
-                            <span>报告生成</span>
-                            <em>将案件生成HTML报告</em>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a onClick={() => this.setState({ importDataModalVisible: true })}>
-                        <i className="indata"></i>
-                        <div className="info">
-                            <span>导入数据</span>
-                            <em>导入第三方数据进行解析</em>
-                        </div>
-                    </a>
-                </li>
-            </menu>
-            <ImportDataModal
-                isLoading={this.state.isLoading}
-                visible={this.state.importDataModalVisible}
-                saveHandle={this.importDataModalSaveHandle}
-                cancelHandle={this.importDataModalCancelHandle} />
-        </div>;
-    }
+                </Spin>
+            </li>
+            <li>
+                <a onClick={() => Modal.info({ title: 'BCP生成', content: '新功能，敬请期待', okText: '确定' })}>
+                    <i className="bcp"></i>
+                    <div className="info">
+                        <span>BCP生成</span>
+                        <em>将报告文件生成BCP文件</em>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a onClick={() => Modal.info({ title: 'BCP上传', content: '新功能，敬请期待', okText: '确定' })}>
+                    <i className="upload"></i>
+                    <div className="info">
+                        <span>BCP上传</span>
+                        <em>将案件上传到指定FTP服务器</em>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a onClick={() => Modal.info({ title: '报告生成', content: '新功能，敬请期待', okText: '确定' })}>
+                    <i className="report"></i>
+                    <div className="info">
+                        <span>报告生成</span>
+                        <em>将案件生成HTML报告</em>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a onClick={() => setImportDataModalVisible(true)}>
+                    <i className="indata"></i>
+                    <div className="info">
+                        <span>导入数据</span>
+                        <em>导入第三方数据进行解析</em>
+                    </div>
+                </a>
+            </li>
+        </menu>
+        <ImportDataModal
+            isLoading={isLoading}
+            visible={importDataModalVisible}
+            saveHandle={importDataModalSaveHandle}
+            cancelHandle={importDataModalCancelHandle} />
+    </div>;
 }
 
 export default Menu;
