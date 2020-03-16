900321942aaa2032f1a254149031260539619f75
cuiyue <cuiyue.msn@hotmail.com>
2020-03-16 16:31:16 +0800

    维护性别、民族、案件类型数据

---
+++ src/schema/CaseType.ts
@@ -0,0 +1,24 @@
+/**
+ * 案件类型数据
+ */
+export const caseType = [
+    { name: '黑客', value: '100' },
+    { name: '淫秽色情', value: '200' },
+    { name: '赌博', value: '300' },
+    { name: '网络盗窃', value: '400' },
+    { name: '侵犯知识产权', value: '500' },
+    { name: '诈骗', value: '600' },
+    { name: '传销', value: '700' },
+    { name: '销售禁售物品', value: '800' },
+    { name: '敲诈勒索', value: '900' },
+    { name: '泄密', value: '1000' },
+    { name: '危害国家安全', value: '1100' },
+    { name: '恐怖', value: '1200' },
+    { name: '邪教', value: '1300' },
+    { name: '抢劫抢夺', value: '1400' },
+    { name: '传统盗窃', value: '1500' },
+    { name: '杀人', value: '1600' },
+    { name: '制毒贩毒', value: '1700' },
+    { name: '侮辱诽谤', value: '1800' },
+    { name: '其他', value: '9900' }
+];
---
+++ src/schema/SexCode.ts
@@ -0,0 +1,6 @@
+export const sexCode = [
+    { name: '未知性别', value: '0' },
+    { name: '男性', value: '1' },
+    { name: '女性', value: '2' },
+    { name: '未说明性别', value: '9' }
+];
