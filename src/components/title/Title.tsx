80f134f12e71a0c8cedb6f3f2de24f6a21de89e3
cuiyue <cuiyue.msn@hotmail.com>
2020-04-09 14:43:08 +0800

    修改Title组件，可指定按钮状态和图标

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -1,31 +1,63 @@
-import React, { PropsWithChildren } from 'react';
+import React, { FC } from 'react';
 import Button from 'antd/lib/button';
 import './Title.less';
 
-interface IProp {
+interface Prop {
     //右侧按钮文本
-    okText?: string | JSX.Element;
+    okText?: string;
     //右侧按钮回调
     onOk?: () => any;
+    //OK按钮属性
+    okButtonProps?: ButtonProps;
     //返回按钮文本
-    returnText?: string | JSX.Element;
+    returnText?: string;
     //返回按钮回调
     onReturn?: () => any;
+    //返回按钮属性
+    returnButtonProps?: ButtonProps;
+}
+
+interface ButtonProps {
+    /**
+     * 是否禁用
+     */
+    disabled?: boolean;
+    /**
+     * 按钮Icon
+     */
+    icon?: string;
 }
 
 /**
- * @description 标题栏
+ * 标题栏
  */
-function Title(props: PropsWithChildren<IProp>) {
-    return <div className="title-bar">
-        <span className="back">
-            {props.returnText ? <Button type="primary" onClick={props.onReturn}>{props.returnText}</Button> : ""}
-        </span>
-        <span className="center-text">{props.children}</span>
-        <span className="btn">
-            {props.okText ? <Button type="primary" onClick={props.onOk}>{props.okText}</Button> : ""}
-        </span>
-    </div>
-}
+const Title: FC<Prop> = (props) => <div className="title-bar">
+    <span className="back">
+        {props.returnText
+            ?
+            <Button
+                type="primary"
+                onClick={props.onReturn}
+                icon={props?.returnButtonProps?.icon}>
+                {props.returnText}
+            </Button>
+            :
+            ""}
+    </span>
+    <span className="center-text">{props.children}</span>
+    <span className="btn">
+        {props.okText
+            ?
+            <Button
+                type="primary"
+                onClick={props.onOk}
+                icon={props?.okButtonProps?.icon}
+                disabled={props?.okButtonProps?.disabled}>
+                {props.okText}
+            </Button>
+            :
+            ""}
+    </span>
+</div>;
 
 export default Title;
