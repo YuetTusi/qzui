d5231925bba611794ad40ef3efef6fadfcf6497d
cuiyue <cuiyue.msn@hotmail.com>
2020-05-06 17:56:56 +0800

    合并test分支代码

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -3,6 +3,9 @@ import { connect } from 'dva';
 import { routerRedux } from 'dva/router';
 import { NavLink } from 'dva/router';
 import { StoreComponent } from '@src/type/model';
+import classnames from 'classnames';
+import config from '@src/config/ui.yaml';
+import iconLogo from './images/icon.png';
 import './Nav.less';
 
 interface Prop extends StoreComponent { }
@@ -12,26 +15,73 @@ interface Prop extends StoreComponent { }
  * @param props 
  */
 const Nav: SFC<Prop> = (props): JSX.Element => {
-    return <nav className="top-nav">
-        <ul>
-            <li onDoubleClick={(e: MouseEvent<HTMLLIElement>) => {
-                const { clientX, clientY } = e;
-                if (clientX < 10 && clientY < 10) {
-                    if ((window as any).toCasePath) {
-                        const { dispatch } = props;
-                        dispatch(routerRedux.push('/settings/case-path'));
-                    } else {
-                        document.body.setAttribute('class', 'eggs');
+
+    const renderBottomLogo = () => {
+
+        if (config.max <= 2) {
+            return <div className="bottom-logo">
+                <img src={iconLogo} className="logo-icon" alt="logo" />
+                <div className="text">
+                    <div>N次方</div>
+                    <div>手机多路取证塔</div>
+                </div>
+            </div>;
+        } else {
+            return null;
+        }
+    };
+
+    return <nav className={classnames('top-nav', { pad: config.max <= 2 })}>
+        <ul className={classnames({ pad: config.max <= 2 })}>
+            <li style={{ display: config.max > 2 ? 'list-item' : 'none' }}
+                onDoubleClick={(e: MouseEvent<HTMLLIElement>) => {
+                    const { clientX, clientY } = e;
+                    if (clientX < 10 && clientY < 10) {
+                        if ((window as any).toCasePath) {
+                            const { dispatch } = props;
+                            dispatch(routerRedux.push('/settings/case-path'));
+                        } else {
+                            document.body.setAttribute('class', 'eggs');
+                        }
                     }
-                }
-            }}><div className="logo"></div></li>
-            <li><NavLink to="/case" replace={true}>案件管理</NavLink></li>
-            <li><NavLink to="/" replace={true} exact={true}>设备取证</NavLink></li>
-            <li><NavLink to="/record" replace={true}>数据解析</NavLink></li>
-            <li><NavLink to="/tools" replace={true}>工具箱</NavLink></li>
-            <li><NavLink to="/operation" replace={true}>操作日志</NavLink></li>
-            <li><NavLink to="/settings/unit" replace={true}>设置</NavLink></li>
+                }}><div className="logo"></div></li>
+            <li>
+                <NavLink to="/case" replace={true}>
+                    {config.max <= 2 ? <i className="case" /> : ''}
+                    <span>案件管理</span></NavLink>
+            </li>
+            <li>
+                <NavLink to="/" replace={true} exact={true}>
+                    {config.max <= 2 ? <i className="dashboard" /> : ''}
+                    <span>设备取证</span>
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/record" replace={true}>
+                    {config.max <= 2 ? <i className="record" /> : ''}
+                    <span>数据解析</span>
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/tools" replace={true}>
+                    {config.max <= 2 ? <i className="tools" /> : ''}
+                    <span>工具箱</span>
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/operation" replace={true}>
+                    {config.max <= 2 ? <i className="operation" /> : ''}
+                    <span>操作日志</span>
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/settings" replace={true}>
+                    {config.max <= 2 ? <i className="settings" /> : ''}
+                    <span>设置</span>
+                </NavLink>
+            </li>
         </ul>
+        {renderBottomLogo()}
     </nav>
 }
 
--- src/view/operation/Index.tsx
+++ src/view/operation/Index.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
+import classnames from 'classnames';
 import Layout from '@src/components/layout/Layout';
 import { Route, NavLink } from 'dva/router';
 import FetchLog from './FetchLog/FetchLog';
 import ParseLog from './ParseLog/ParseLog';
+import config from '@src/config/ui.yaml';
 import './Index.less';
 
 interface Prop { }
@@ -13,13 +15,13 @@ interface Prop { }
  */
 const Index: FC<Prop> = (props) => {
     return <Layout>
-        <div className="operation-root">
-            <menu className="log-menu">
+        <div className={classnames('operation-root', { pad: config.max <= 2 })}>
+            <menu className={classnames('log-menu', { pad: config.max <= 2 })}>
                 <ul>
                     <li>
                         <NavLink to="/operation" replace={true} exact={true} className="fetch">
                             <div>
-                                <i title="采集日志" />
+                                {config.max <= 2 ? '' : <i title="采集日志" />}
                                 <span>采集日志</span>
                             </div>
                         </NavLink>
@@ -27,7 +29,7 @@ const Index: FC<Prop> = (props) => {
                     <li>
                         <NavLink to="/operation/parse-log" replace={true} className="parse">
                             <div>
-                                <i title="解析日志" />
+                                {config.max <= 2 ? '' : <i title="解析日志" />}
                                 <span>解析日志</span>
                             </div>
                         </NavLink>
