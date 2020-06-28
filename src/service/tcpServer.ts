3a7920179240054c77d8b162e4d9a9885e02c2aa
cuiyue <cuiyue.msn@hotmail.com>
2020-06-28 09:54:30 +0800

    增加错误日志

--- src/service/tcpServer.ts
+++ src/service/tcpServer.ts
@@ -1,7 +1,6 @@
-
-import { EventEmitter } from 'events';
 import net, { Socket } from 'net';
 import { stick } from 'stickpackage';
+import logger from '@utils/log';
 
 interface SocketPool {
     port: number;
@@ -11,33 +10,29 @@ interface SocketPool {
 
 const stack: any = new stick(1024).setReadIntBE('32');
 const pool = new Map<string, SocketPool>();
+const server = net.createServer();
 
-let server = net.createServer(() => {
-    console.log('已连接');
-});
+server.on('connection', (socket: Socket) => {
 
-server.on('connection', (socket) => {
+    logger.info(`Socket接入, 端口号: ${socket.remotePort}`);
 
-    console.log('有socket接入');
-
-    socket.on('data', (chunk) => {
+    socket.on('data', (chunk: Buffer) => {
         stack.__socket__ = socket;
         stack.putData(chunk);
     })
 
     socket.on('error', (err) => {
         removeSocketByPort(pool, socket.remotePort!);
-        console.log(pool.size);
-        console.log(err.message);
+        logger.info(`Socket断开, 端口号: ${socket.remotePort}, 错误消息: ${err.message}`);
     });
 });
 
 server.on('close', () => {
-    console.log('服务已关闭');
+    console.log('TCP服务已关闭');
 });
 
 server.on('error', (err) => {
-    console.log(err.message);
+    logger.info(`TCP服务中断,错误消息: ${err.message}`);
 });
 
 stack.onData((data: Buffer) => {
@@ -51,6 +46,7 @@ stack.onData((data: Buffer) => {
     let origin = body.toString();
     let json = JSON.parse(origin);
     console.log('收到数据: ', json.type);
+    server.emit(json.type, json);
 
     let socket = stack.__socket__;
 
@@ -61,7 +57,6 @@ stack.onData((data: Buffer) => {
             port: socket.remotePort!,
             socket
         });
-        console.log(pool.size);
     }
 });
 
@@ -70,7 +65,7 @@ stack.onData((data: Buffer) => {
  * @param type socket类型
  * @param data 消息数据(JSON类型)
  */
-function send(type: string, data: any) {
+function send(type: string, data: Partial<Record<string, any>>) {
     // 回复数据
     const body = Buffer.from(JSON.stringify(data));
     // 写入包头, 也就是4字节大小, 该大小指向后面的json数据的长度, 也就是这里的body
