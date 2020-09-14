6db94c6c76a5a2fb42f5e32dbdc0d310284c7e68
cuiyue <cuiyue.msn@hotmail.com>
2020-09-14 13:35:14 +0800

    更换列顺序

--- main.js
+++ main.js
@@ -31,24 +31,24 @@ let httpServerIsRunning = false; //是否已启动HttpServer
 
 //#region 读配置文件
 if (mode === 'development') {
-    config = yaml.safeLoad(fs.readFileSync(path.join(appPath, 'src/config/ui.yaml'), 'utf8'));
+	config = yaml.safeLoad(fs.readFileSync(path.join(appPath, 'src/config/ui.yaml'), 'utf8'));
 } else {
-    try {
-        let chunk = fs.readFileSync(path.join(appPath, '../config/conf'), 'utf8');
-        const decipher = crypto.createDecipher('rc4', KEY);
-        let conf = decipher.update(chunk, 'hex', 'utf8');
-        conf += decipher.final('utf8');
-        config = yaml.safeLoad(conf);
-    } catch (error) {
-        dialog.showErrorBox('启动失败', '配置文件读取失败，请联系技术支持');
-        app.exit(0);
-    }
+	try {
+		let chunk = fs.readFileSync(path.join(appPath, '../config/conf'), 'utf8');
+		const decipher = crypto.createDecipher('rc4', KEY);
+		let conf = decipher.update(chunk, 'hex', 'utf8');
+		conf += decipher.final('utf8');
+		config = yaml.safeLoad(conf);
+	} catch (error) {
+		dialog.showErrorBox('启动失败', '配置文件读取失败，请联系技术支持');
+		app.exit(0);
+	}
 }
 //#endregion
 
 var notifier = new WindowsBalloon({
-    withFallback: false,
-    customPath: undefined
+	withFallback: false,
+	customPath: undefined
 });
 
 //# 配置Http服务器相关
@@ -59,282 +59,284 @@ server.use(cors({ optionsSuccessStatus: 200 }));
  * 销毁所有窗口
  */
 function destroyAllWindow() {
-    if (fetchRecordWindow !== null) {
-        fetchRecordWindow.destroy();
-        fetchRecordWindow = null;
-    }
-    if (sqliteWindow !== null) {
-        sqliteWindow.destroy();
-        sqliteWindow = null;
-    }
-    if (timerWindow !== null) {
-        timerWindow.destroy();
-        timerWindow = null;
-    }
-    if (mainWindow !== null) {
-        mainWindow.destroy();
-        mainWindow = null;
-    }
+	if (fetchRecordWindow !== null) {
+		fetchRecordWindow.destroy();
+		fetchRecordWindow = null;
+	}
+	if (sqliteWindow !== null) {
+		sqliteWindow.destroy();
+		sqliteWindow = null;
+	}
+	if (timerWindow !== null) {
+		timerWindow.destroy();
+		timerWindow = null;
+	}
+	if (mainWindow !== null) {
+		mainWindow.destroy();
+		mainWindow = null;
+	}
 }
 
 app.on('before-quit', () => {
-    //移除mainWindow上的listeners
-    mainWindow.removeAllListeners('close');
+	//移除mainWindow上的listeners
+	mainWindow.removeAllListeners('close');
 });
 
 const instanceLock = app.requestSingleInstanceLock();
 if (!instanceLock) {
-    app.quit(0);
+	app.quit(0);
 } else {
-    app.on('second-instance', (event, commandLine, workingDirectory) => {
-        //单例应用
-        if (mainWindow) {
-            if (mainWindow.isMinimized()) {
-                mainWindow.restore();
-            }
-            mainWindow.focus();
-            mainWindow.show();
-        }
-    });
+	app.on('second-instance', (event, commandLine, workingDirectory) => {
+		//单例应用
+		if (mainWindow) {
+			if (mainWindow.isMinimized()) {
+				mainWindow.restore();
+			}
+			mainWindow.focus();
+			mainWindow.show();
+		}
+	});
 
-    app.on('ready', () => {
-        mainWindow = new BrowserWindow({
-            title: config.title || '北京万盛华通科技有限公司',
-            width: config.windowWidth || 1280, //主窗体宽
-            height: config.windowHeight || 800, //主窗体高
-            fullscreen: false, //是否全屏
-            autoHideMenuBar: true, //隐藏主窗口菜单
-            center: config.center || true, //居中显示
-            minHeight: config.minHeight || 768, //最小高度
-            minWidth: config.minWidth || 960, //最小宽度
-            webPreferences: {
-                webSecurity: false,
-                nodeIntegration: true,
-                javascript: true
-            }
-        });
+	app.on('ready', () => {
+		mainWindow = new BrowserWindow({
+			title: config.title || '北京万盛华通科技有限公司',
+			width: config.windowWidth || 1280, //主窗体宽
+			height: config.windowHeight || 800, //主窗体高
+			fullscreen: false, //是否全屏
+			autoHideMenuBar: true, //隐藏主窗口菜单
+			center: config.center || true, //居中显示
+			minHeight: config.minHeight || 768, //最小高度
+			minWidth: config.minWidth || 960, //最小宽度
+			webPreferences: {
+				webSecurity: false,
+				nodeIntegration: true,
+				javascript: true
+			}
+		});
 
-        if (mode === 'development') {
-            mainWindow.webContents.openDevTools();
-            mainWindow.loadURL(config.devPageUrl);
-        } else {
-            if (!config.publishPage) {
-                config.publishPage = './dist/index.html';
-            }
-            mainWindow.loadURL(`file://${path.join(__dirname, config.publishPage)}`);
-        }
+		if (mode === 'development') {
+			mainWindow.webContents.openDevTools();
+			mainWindow.loadURL(config.devPageUrl);
+		} else {
+			if (!config.publishPage) {
+				config.publishPage = './dist/index.html';
+			}
+			mainWindow.loadURL(`file://${path.join(__dirname, config.publishPage)}`);
+		}
 
-        mainWindow.webContents.on('did-finish-load', () => {
-            const { width, height } = mainWindow.getBounds();
-            if (timerWindow) {
-                timerWindow.reload();
-            }
-            if (sqliteWindow) {
-                sqliteWindow.reload();
-            }
-            if (fetchRecordWindow) {
-                fetchRecordWindow.reload();
-            }
+		mainWindow.webContents.on('did-finish-load', () => {
+			const { width, height } = mainWindow.getBounds();
+			if (timerWindow) {
+				timerWindow.reload();
+			}
+			if (sqliteWindow) {
+				sqliteWindow.reload();
+			}
+			if (fetchRecordWindow) {
+				fetchRecordWindow.reload();
+			}
 
-            fetchProcess = spawn(config.fetchExe || 'n_fetch.exe', {
-                cwd: path.join(appPath, '../../../', config.fetchPath)
-            });
-            fetchProcess.on('error', () => {
-                console.log('采集程序启动失败');
-                fetchProcess = null;
-            });
-            parseProcess = spawn(config.parseExe || 'parse.exe', {
-                cwd: path.join(appPath, '../../../', config.parsePath)
-            });
-            parseProcess.on('error', () => {
-                console.log('解析程序启动失败');
-                parseProcess = null;
-            });
-            //向mainWindow发送窗口宽高值
-            mainWindow.webContents.send('window-resize', width, height);
-            if (!httpServerIsRunning) {
-                server.use(api(mainWindow.webContents));
-                server.listen(config.httpPort || 9900, () => {
-                    httpServerIsRunning = true;
-                    console.log(`HTTP服务启动在端口${config.httpPort || 9900}`);
-                });
-            }
-        });
+			fetchProcess = spawn(config.fetchExe || 'n_fetch.exe', {
+				cwd: path.join(appPath, '../../../', config.fetchPath)
+			});
+			fetchProcess.on('error', () => {
+				console.log('采集程序启动失败');
+				fetchProcess = null;
+			});
+			parseProcess = spawn(config.parseExe || 'parse.exe', {
+				cwd: path.join(appPath, '../../../', config.parsePath)
+			});
+			parseProcess.on('error', () => {
+				console.log('解析程序启动失败');
+				parseProcess = null;
+			});
+			//向mainWindow发送窗口宽高值
+			mainWindow.webContents.send('window-resize', width, height);
 
-        mainWindow.on(
-            'resize',
-            throttle(
-                (event) => {
-                    event.preventDefault();
-                    const { width, height } = mainWindow.getBounds();
-                    mainWindow.webContents.send('window-resize', width, height);
-                },
-                1000,
-                { leading: false, trailing: true }
-            )
-        );
+			if (!httpServerIsRunning) {
+				//启动HTTP服务
+				server.use(api(mainWindow.webContents));
+				server.listen(config.httpPort || 9900, () => {
+					httpServerIsRunning = true;
+					console.log(`HTTP服务启动在端口${config.httpPort || 9900}`);
+				});
+			}
+		});
 
-        mainWindow.on('close', (event) => {
-            //关闭事件到mainWindow中去处理
-            event.preventDefault();
-            mainWindow.webContents.send('will-close');
-        });
+		mainWindow.on(
+			'resize',
+			throttle(
+				(event) => {
+					event.preventDefault();
+					const { width, height } = mainWindow.getBounds();
+					mainWindow.webContents.send('window-resize', width, height);
+				},
+				1000,
+				{ leading: false, trailing: true }
+			)
+		);
 
-        timerWindow = new BrowserWindow({
-            title: '计时服务',
-            width: 600,
-            height: 400,
-            show: false,
-            webPreferences: {
-                nodeIntegration: true,
-                javascript: true
-            }
-        });
-        timerWindow.loadURL(`file://${path.join(__dirname, './src/renderer/timer/timer.html')}`);
+		mainWindow.on('close', (event) => {
+			//关闭事件到mainWindow中去处理
+			event.preventDefault();
+			mainWindow.webContents.send('will-close');
+		});
 
-        sqliteWindow = new BrowserWindow({
-            title: 'SQLite',
-            width: 600,
-            height: 400,
-            show: false,
-            webPreferences: {
-                nodeIntegration: true,
-                javascript: true
-            }
-        });
-        sqliteWindow.loadURL(`file://${path.join(__dirname, './src/renderer/sqlite/sqlite.html')}`);
+		timerWindow = new BrowserWindow({
+			title: '计时服务',
+			width: 600,
+			height: 400,
+			show: false,
+			webPreferences: {
+				nodeIntegration: true,
+				javascript: true
+			}
+		});
+		timerWindow.loadURL(`file://${path.join(__dirname, './src/renderer/timer/timer.html')}`);
 
-        fetchRecordWindow = new BrowserWindow({
-            width: 600,
-            height: 400,
-            show: false,
-            webPreferences: {
-                nodeIntegration: true,
-                javascript: true
-            }
-        });
-        fetchRecordWindow.loadURL(
-            `file://${path.join(__dirname, './src/renderer/fetchRecord/fetchRecord.html')}`
-        );
-        if (mode === 'development') {
-            timerWindow.webContents.openDevTools();
-            sqliteWindow.webContents.openDevTools();
-            fetchRecordWindow.webContents.openDevTools();
-        }
+		sqliteWindow = new BrowserWindow({
+			title: 'SQLite',
+			width: 600,
+			height: 400,
+			show: false,
+			webPreferences: {
+				nodeIntegration: true,
+				javascript: true
+			}
+		});
+		sqliteWindow.loadURL(`file://${path.join(__dirname, './src/renderer/sqlite/sqlite.html')}`);
 
-        // #生产模式屏蔽快捷键（发布把注释放开）
-        // if (mode !== 'development') {
-        //     globalShortcut.register('Control+R', () => { });
-        //     globalShortcut.register('Control+Shift+R', () => { });
-        //     globalShortcut.register('CommandOrControl+Shift+I', () => { });
-        // }
-        // #默认菜单置空（发布把注释放开）
-        // if (mode !== 'development') {
-        //     Menu.setApplicationMenu(null);
-        // }
-    });
+		fetchRecordWindow = new BrowserWindow({
+			width: 600,
+			height: 400,
+			show: false,
+			webPreferences: {
+				nodeIntegration: true,
+				javascript: true
+			}
+		});
+		fetchRecordWindow.loadURL(
+			`file://${path.join(__dirname, './src/renderer/fetchRecord/fetchRecord.html')}`
+		);
+		if (mode === 'development') {
+			timerWindow.webContents.openDevTools();
+			sqliteWindow.webContents.openDevTools();
+			fetchRecordWindow.webContents.openDevTools();
+		}
+
+		// #生产模式屏蔽快捷键（发布把注释放开）
+		// if (mode !== 'development') {
+		//     globalShortcut.register('Control+R', () => { });
+		//     globalShortcut.register('Control+Shift+R', () => { });
+		//     globalShortcut.register('CommandOrControl+Shift+I', () => { });
+		// }
+		// #默认菜单置空（发布把注释放开）
+		// if (mode !== 'development') {
+		//     Menu.setApplicationMenu(null);
+		// }
+	});
 }
 
 //#region 消息事件
 
 //显示原生系统消息
 ipcMain.on('show-notice', (event, args) => {
-    notifier.notify({
-        sound: true,
-        type: 'info',
-        title: args.title || '消息',
-        message: args.message || '有消息反馈请查阅'
-    });
+	notifier.notify({
+		sound: true,
+		type: 'info',
+		title: args.title || '消息',
+		message: args.message || '有消息反馈请查阅'
+	});
 });
 
 //显示notification消息,参数为消息文本
 ipcMain.on('show-notification', (event, args) => {
-    mainWindow.webContents.send('show-notification', args);
+	mainWindow.webContents.send('show-notification', args);
 });
 
 ipcMain.on('do-close', (event) => {
-    //mainWindow通知退出程序
-    if (process.platform !== 'darwin') {
-        globalShortcut.unregisterAll();
-        destroyAllWindow();
-        if (fetchProcess !== null) {
-            fetchProcess.kill(); //杀掉采集进程
-        }
-        if (parseProcess !== null) {
-            parseProcess.kill(); //杀掉解析进程
-        }
-        app.exit(0);
-    }
+	//mainWindow通知退出程序
+	if (process.platform !== 'darwin') {
+		globalShortcut.unregisterAll();
+		destroyAllWindow();
+		if (fetchProcess !== null) {
+			fetchProcess.kill(); //杀掉采集进程
+		}
+		if (parseProcess !== null) {
+			parseProcess.kill(); //杀掉解析进程
+		}
+		app.exit(0);
+	}
 });
 
 //启动&停止计时
 ipcMain.on('time', (event, usb, isStart) => {
-    if (timerWindow !== null) {
-        timerWindow.webContents.send('time', usb, isStart);
-    }
+	if (timerWindow !== null) {
+		timerWindow.webContents.send('time', usb, isStart);
+	}
 });
 //向主窗口发送计时时间
 ipcMain.on('receive-time', (event, usb, timeString) => {
-    // console.log(`${usb}:${timeString}`);
-    if (mainWindow && mainWindow.webContents !== null) {
-        mainWindow.webContents.send('receive-time', usb, timeString);
-    }
+	// console.log(`${usb}:${timeString}`);
+	if (mainWindow && mainWindow.webContents !== null) {
+		mainWindow.webContents.send('receive-time', usb, timeString);
+	}
 });
 //执行SQLite查询单位表
 ipcMain.on('query-db', (event, ...args) => {
-    sqliteWindow.webContents.send('query-db', args);
+	sqliteWindow.webContents.send('query-db', args);
 });
 //SQLite查询结果
 ipcMain.on('query-db-result', (event, result) => {
-    mainWindow.webContents.send('query-db-result', result);
+	mainWindow.webContents.send('query-db-result', result);
 });
 //执行SQLite查询BcpConf表
 ipcMain.on('query-bcp-conf', (event, ...args) => {
-    sqliteWindow.webContents.send('query-bcp-conf', args);
+	sqliteWindow.webContents.send('query-bcp-conf', args);
 });
 //查询BcpConf表结果
 ipcMain.on('query-bcp-conf-result', (event, result) => {
-    mainWindow.webContents.send('query-bcp-conf-result', result);
+	mainWindow.webContents.send('query-bcp-conf-result', result);
 });
 
 //SQLite更新BcpConf表
 ipcMain.on('update-bcp-conf', (event, ...args) => {
-    sqliteWindow.webContents.send('update-bcp-conf', args);
+	sqliteWindow.webContents.send('update-bcp-conf', args);
 });
 //SQLite更新BcpConf表结果
 ipcMain.on('update-bcp-conf-result', (event, result) => {
-    mainWindow.webContents.send('update-bcp-conf-result', result);
+	mainWindow.webContents.send('update-bcp-conf-result', result);
 });
 //发送进度消息
 ipcMain.on('fetch-progress', (event, arg) => {
-    fetchRecordWindow.webContents.send('fetch-progress', arg);
+	fetchRecordWindow.webContents.send('fetch-progress', arg);
 });
 //采集完成发送USB号及日志数据
 ipcMain.on('fetch-finish', (event, usb, log) => {
-    fetchRecordWindow.webContents.send('fetch-finish', usb, log);
+	fetchRecordWindow.webContents.send('fetch-finish', usb, log);
 });
 //清除usb序号对应的采集记录
 ipcMain.on('progress-clear', (event, usb) => {
-    fetchRecordWindow.webContents.send('progress-clear', usb);
+	fetchRecordWindow.webContents.send('progress-clear', usb);
 });
 //获取当前USB序号的采集进度数据
 ipcMain.on('get-fetch-progress', (event, usb) => {
-    fetchRecordWindow.webContents.send('get-fetch-progress', usb);
+	fetchRecordWindow.webContents.send('get-fetch-progress', usb);
 });
 //获取当前USB序号最新一条进度消息
 ipcMain.on('get-last-progress', (event, usb) => {
-    fetchRecordWindow.webContents.send('get-last-progress', usb);
+	fetchRecordWindow.webContents.send('get-last-progress', usb);
 });
 //消息发回LiveModal以显示采集进度
 ipcMain.on('receive-fetch-progress', (event, fetchRecords) => {
-    mainWindow.webContents.send('receive-fetch-progress', fetchRecords);
+	mainWindow.webContents.send('receive-fetch-progress', fetchRecords);
 });
 //消息发回FetchInfo.tsx组件以显示最新一条进度
 ipcMain.on('receive-fetch-last-progress', (event, fetchRecord) => {
-    mainWindow.webContents.send('receive-fetch-last-progress', fetchRecord);
+	mainWindow.webContents.send('receive-fetch-last-progress', fetchRecord);
 });
 //将FetchLog数据发送给入库
 ipcMain.on('save-fetch-log', (event, log) => {
-    mainWindow.webContents.send('save-fetch-log', log);
+	mainWindow.webContents.send('save-fetch-log', log);
 });
 //#endregion
--- src/view/settings/CheckManage/columns.tsx
+++ src/view/settings/CheckManage/columns.tsx
@@ -10,108 +10,106 @@ import { FetchData } from '@src/schema/socket/FetchData';
  * @param dispatch 派发方法
  */
 export function getColumns<T>(dispatch: Dispatch<T>): ColumnGroupProps[] {
-    const columns = [
-        {
-            title: '序列号',
-            dataIndex: 'serial',
-            key: 'serial'
-        },
-        // {
-        //     title: '案件名称',
-        //     dataIndex: 'caseName',
-        //     key: 'caseName',
-        //     render(val: string) {
-        //         const [caseName] = val.split('_');
-        //         return <span>{caseName}</span>;
-        //     }
-        // },
-        {
-            title: '手机名称',
-            dataIndex: 'mobileName',
-            key: 'mobileName',
-            render(val: string) {
-                const [caseName] = val.split('_');
-                return <span>{caseName}</span>;
-            }
-        },
-        {
-            title: '姓名',
-            dataIndex: 'mobileHolder',
-            key: 'mobileHolder'
-        },
-        {
-            title: '身份证/军官证号',
-            dataIndex: 'credential',
-            key: 'credential'
-        },
-        {
-            title: '设备手机号',
-            dataIndex: 'note',
-            key: 'note'
-        },
-        {
-            title: '手机编号',
-            dataIndex: 'mobileNo',
-            key: 'mobileNo'
-        },
-        {
-            title: '编辑',
-            dataIndex: 'serial',
-            key: 'serial',
-            width: '60px',
-            align: 'center',
-            render: (val: string, record: FetchData) => {
-                return (
-                    <a
-                        onClick={(e: MouseEvent<HTMLAnchorElement>) => {
-                            e.stopPropagation();
-                            dispatch({ type: 'checkManage/queryDataBySerial', payload: val });
-                        }}
-                    >
-                        编辑
-                    </a>
-                );
-            }
-        },
-        {
-            title: '点验时间',
-            dataIndex: 'updatedAt',
-            key: 'updatedAt',
-            width: '160px',
-            align: 'center',
-            render(val: Date) {
-                return <span>{moment(val).format('YYYY-MM-DD HH:mm:ss')}</span>;
-            }
-        },
-        {
-            title: '删除',
-            key: 'serial',
-            width: '60px',
-            align: 'center',
-            render: (val: string, record: FetchData) => {
-                return (
-                    <a
-                        onClick={(e: MouseEvent<HTMLAnchorElement>) => {
-                            e.preventDefault();
-                            Modal.confirm({
-                                title: '删除',
-                                content: `删除序列号为「${record.serial}」的数据？`,
-                                okText: '是',
-                                cancelText: '否',
-                                onOk() {
-                                    dispatch({
-                                        type: 'checkManage/delCheckData',
-                                        payload: record
-                                    });
-                                }
-                            });
-                        }}
-                    >
-                        删除
-                    </a>
-                );
-            }
-        }
-    ];
-    return columns;
+	const columns = [
+		{
+			title: '序列号',
+			dataIndex: 'serial',
+			key: 'serial'
+		},
+		// {
+		//     title: '案件名称',
+		//     dataIndex: 'caseName',
+		//     key: 'caseName',
+		//     render(val: string) {
+		//         const [caseName] = val.split('_');
+		//         return <span>{caseName}</span>;
+		//     }
+		// },
+		{
+			title: '手机名称',
+			dataIndex: 'mobileName',
+			key: 'mobileName',
+			render(val: string) {
+				const [caseName] = val.split('_');
+				return <span>{caseName}</span>;
+			}
+		},
+		{
+			title: '姓名',
+			dataIndex: 'mobileHolder',
+			key: 'mobileHolder'
+		},
+		{
+			title: '身份证/军官证号',
+			dataIndex: 'credential',
+			key: 'credential'
+		},
+		{
+			title: '设备手机号',
+			dataIndex: 'note',
+			key: 'note'
+		},
+		{
+			title: '手机编号',
+			dataIndex: 'mobileNo',
+			key: 'mobileNo'
+		},
+		{
+			title: '点验时间',
+			dataIndex: 'updatedAt',
+			key: 'updatedAt',
+			width: '160px',
+			align: 'center',
+			render(val: Date) {
+				return <span>{moment(val).format('YYYY-MM-DD HH:mm:ss')}</span>;
+			}
+		},
+		{
+			title: '编辑',
+			dataIndex: 'serial',
+			key: 'serial',
+			width: '60px',
+			align: 'center',
+			render: (val: string, record: FetchData) => {
+				return (
+					<a
+						onClick={(e: MouseEvent<HTMLAnchorElement>) => {
+							e.stopPropagation();
+							dispatch({ type: 'checkManage/queryDataBySerial', payload: val });
+						}}>
+						编辑
+					</a>
+				);
+			}
+		},
+		{
+			title: '删除',
+			key: 'serial',
+			width: '60px',
+			align: 'center',
+			render: (val: string, record: FetchData) => {
+				return (
+					<a
+						onClick={(e: MouseEvent<HTMLAnchorElement>) => {
+							e.preventDefault();
+							Modal.confirm({
+								title: '删除',
+								content: `删除序列号为「${record.serial}」的数据？`,
+								okText: '是',
+								cancelText: '否',
+								onOk() {
+									dispatch({
+										type: 'checkManage/delCheckData',
+										payload: record
+									});
+								}
+							});
+						}}>
+						删除
+					</a>
+				);
+			}
+		}
+	];
+	return columns;
 }
