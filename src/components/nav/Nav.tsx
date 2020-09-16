510f4d65e11e850007cca41be24c85e8ff73547a
cuiyue <cuiyue.msn@hotmail.com>
2020-09-16 14:43:30 +0800

    修改标题文字

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -12,98 +12,123 @@ import './Nav.less';
 
 const config = helper.readConf();
 
-interface Prop extends StoreComponent { }
+interface Prop extends StoreComponent {}
 
 /**
  * 导航菜单
- * @param props 
+ * @param props
  */
 const Nav: SFC<Prop> = (props): JSX.Element => {
+	const renderBottomLogo = () => {
+		if (config.max <= 2) {
+			return (
+				<div className="bottom-logo">
+					<img src={iconLogo} className="logo-icon" alt="logo" />
+					<div className="text">
+						<div>N次方</div>
+						<div>手机多路取证塔</div>
+					</div>
+				</div>
+			);
+		} else {
+			return null;
+		}
+	};
 
-    const renderBottomLogo = () => {
+	return (
+		<nav
+			className={classnames('top-nav', { pad: config.max <= 2 })}
+			onContextMenu={(event: MouseEvent<HTMLElement>) => {
+				event.preventDefault();
+				const { clientX, clientY } = event;
+				const { dispatch } = props;
 
-        if (config.max <= 2) {
-            return <div className="bottom-logo">
-                <img src={iconLogo} className="logo-icon" alt="logo" />
-                <div className="text">
-                    <div>N次方</div>
-                    <div>手机多路取证塔</div>
-                </div>
-            </div>;
-        } else {
-            return null;
-        }
-    };
-
-    return <nav
-        className={classnames('top-nav', { pad: config.max <= 2 })}
-        onContextMenu={(event: MouseEvent<HTMLElement>) => {
-            event.preventDefault();
-            const { clientX, clientY } = event;
-            const { dispatch } = props;
-
-            if (clientX < 20 && clientY < 20) {
-                const ctxMenu = hiddenMenu([
-                    { label: '采集日志管理', click: () => dispatch(routerRedux.push('/operation?role=admin')) },
-                    { label: '解析日志管理', click: () => dispatch(routerRedux.push('/operation/parse-log?role=admin')) },
-                    { label: 'BCP生成信息配置', click: () => dispatch(routerRedux.push('/settings/bcp-conf')) },
-                    { label: '历史记录清除', click: () => dispatch(routerRedux.push('/settings/input-history')) },
-                    { label: '单位设置清除', click: () => dispatch(routerRedux.push('/settings/clear-unit')) },
-                    { label: '显示DevTools', click: () => remote.getCurrentWebContents().openDevTools() },
-                    { label: '刷新窗口', click: () => remote.getCurrentWindow().reload() }
-                ]);
-                ctxMenu.popup({ x: clientX, y: clientY });
-            }
-        }}>
-        <ul className={classnames({ pad: config.max <= 2 })}>
-            <li style={{ display: config.max > 2 ? 'list-item' : 'none' }}
-                onDoubleClick={(e: MouseEvent<HTMLLIElement>) => {
-                    const { clientX, clientY } = e;
-                    if (clientX < 10 && clientY < 10) {
-                        document.body.setAttribute('class', 'eggs');
-                        setTimeout(() => {
-                            document.body.removeAttribute('class');
-                        }, 2000);
-                    }
-                }}><div className="logo"></div></li>
-            <li>
-                <NavLink to="/case" replace={true}>
-                    {config.max <= 2 ? <i className="case" /> : ''}
-                    <span>案件管理</span></NavLink>
-            </li>
-            <li>
-                <NavLink to="/" replace={true} exact={true}>
-                    {config.max <= 2 ? <i className="dashboard" /> : ''}
-                    <span>设备取证</span>
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/record" replace={true}>
-                    {config.max <= 2 ? <i className="record" /> : ''}
-                    <span>数据解析</span>
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/tools" replace={true}>
-                    {config.max <= 2 ? <i className="tools" /> : ''}
-                    <span>工具箱</span>
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/operation" replace={true}>
-                    {config.max <= 2 ? <i className="operation" /> : ''}
-                    <span>操作日志</span>
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/settings" replace={true}>
-                    {config.max <= 2 ? <i className="settings" /> : ''}
-                    <span>设置</span>
-                </NavLink>
-            </li>
-        </ul>
-        {renderBottomLogo()}
-    </nav>
-}
+				if (clientX < 20 && clientY < 20) {
+					const ctxMenu = hiddenMenu([
+						{
+							label: '采集日志管理',
+							click: () => dispatch(routerRedux.push('/operation?role=admin'))
+						},
+						{
+							label: '解析日志管理',
+							click: () =>
+								dispatch(routerRedux.push('/operation/parse-log?role=admin'))
+						},
+						{
+							label: '软硬件信息配置',
+							click: () => dispatch(routerRedux.push('/settings/bcp-conf'))
+						},
+						{
+							label: '历史记录清除',
+							click: () => dispatch(routerRedux.push('/settings/input-history'))
+						},
+						{
+							label: '单位设置清除',
+							click: () => dispatch(routerRedux.push('/settings/clear-unit'))
+						},
+						{
+							label: '显示DevTools',
+							click: () => remote.getCurrentWebContents().openDevTools()
+						},
+						{ label: '刷新窗口', click: () => remote.getCurrentWindow().reload() }
+					]);
+					ctxMenu.popup({ x: clientX, y: clientY });
+				}
+			}}>
+			<ul className={classnames({ pad: config.max <= 2 })}>
+				<li
+					style={{ display: config.max > 2 ? 'list-item' : 'none' }}
+					onDoubleClick={(e: MouseEvent<HTMLLIElement>) => {
+						const { clientX, clientY } = e;
+						if (clientX < 10 && clientY < 10) {
+							document.body.setAttribute('class', 'eggs');
+							setTimeout(() => {
+								document.body.removeAttribute('class');
+							}, 2000);
+						}
+					}}>
+					<div className="logo"></div>
+				</li>
+				<li>
+					<NavLink to="/case" replace={true}>
+						{config.max <= 2 ? <i className="case" /> : ''}
+						<span>案件管理</span>
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/" replace={true} exact={true}>
+						{config.max <= 2 ? <i className="dashboard" /> : ''}
+						<span>设备取证</span>
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/record" replace={true}>
+						{config.max <= 2 ? <i className="record" /> : ''}
+						<span>数据解析</span>
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/tools" replace={true}>
+						{config.max <= 2 ? <i className="tools" /> : ''}
+						<span>工具箱</span>
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/operation" replace={true}>
+						{config.max <= 2 ? <i className="operation" /> : ''}
+						<span>操作日志</span>
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/settings" replace={true}>
+						{config.max <= 2 ? <i className="settings" /> : ''}
+						<span>设置</span>
+					</NavLink>
+				</li>
+			</ul>
+			{renderBottomLogo()}
+		</nav>
+	);
+};
 
 export default connect(() => ({ nav: null }))(Nav);
--- src/view/settings/BcpConf/BcpConf.tsx
+++ src/view/settings/BcpConf/BcpConf.tsx
@@ -17,151 +17,177 @@ const { Item } = Form;
  * 数据在SQLite BcpConf表中维护
  */
 const BcpConf = Form.create<Prop>({ name: 'bcpConfForm' })((props: Prop) => {
+	const { getFieldDecorator } = props.form;
 
-    const { getFieldDecorator } = props.form;
+	let formValue = useRef<FormValue>();
+	const [data, setData] = useState<any>({});
 
-    let formValue = useRef<FormValue>();
-    const [data, setData] = useState<any>({});
+	/**
+	 * 查询结果Handle
+	 */
+	const queryBcpConfResultHandle = (event: IpcRendererEvent, result: Record<string, any>) => {
+		if (result.success) {
+			setData(result.data.row);
+		}
+	};
+	/**
+	 * 更新BcpConf结果
+	 */
+	const updateBcpConfResultHandle = (event: IpcRendererEvent, result: Record<string, any>) => {
+		const { current } = formValue;
+		if (result.success) {
+			message.success('保存成功');
+			console.log(current);
+			localStorage.setItem(
+				'manufacturer',
+				helper.isNullOrUndefinedOrEmptyString(current?.manufacturer)
+					? ''
+					: current?.manufacturer!
+			);
+			localStorage.setItem(
+				'security_software_orgcode',
+				helper.isNullOrUndefinedOrEmptyString(current?.securitySoftwareOrgCode)
+					? ''
+					: current?.securitySoftwareOrgCode!
+			);
+			localStorage.setItem(
+				'materials_name',
+				helper.isNullOrUndefinedOrEmptyString(current?.materialsName)
+					? ''
+					: current?.materialsName!
+			);
+			localStorage.setItem(
+				'materials_model',
+				helper.isNullOrUndefinedOrEmptyString(current?.materialsModel)
+					? ''
+					: current?.materialsModel!
+			);
+			localStorage.setItem(
+				'materials_hardware_version',
+				helper.isNullOrUndefinedOrEmptyString(current?.materialsHardwareVersion)
+					? ''
+					: current?.materialsHardwareVersion!
+			);
+			localStorage.setItem(
+				'materials_software_version',
+				helper.isNullOrUndefinedOrEmptyString(current?.materialsSoftwareVersion)
+					? ''
+					: current?.materialsSoftwareVersion!
+			);
+			localStorage.setItem(
+				'materials_serial',
+				helper.isNullOrUndefinedOrEmptyString(current?.materialsSerial)
+					? ''
+					: current?.materialsSerial!
+			);
+			localStorage.setItem(
+				'ip_address',
+				helper.isNullOrUndefinedOrEmptyString(current?.ipAddress) ? '' : current?.ipAddress!
+			);
+		} else {
+			message.error('保存失败');
+		}
+	};
 
-    /**
-    * 查询结果Handle
-    */
-    const queryBcpConfResultHandle = (event: IpcRendererEvent, result: Record<string, any>) => {
-        if (result.success) {
-            setData(result.data.row);
-        }
-    };
-    /**
-     * 更新BcpConf结果
-     */
-    const updateBcpConfResultHandle = (event: IpcRendererEvent, result: Record<string, any>) => {
-        const { current } = formValue;
-        if (result.success) {
-            message.success('保存成功');
-            console.log(current);
-            localStorage.setItem('manufacturer', helper.isNullOrUndefinedOrEmptyString(current?.manufacturer) ? '' : current?.manufacturer!);
-            localStorage.setItem('security_software_orgcode', helper.isNullOrUndefinedOrEmptyString(current?.securitySoftwareOrgCode) ? '' : current?.securitySoftwareOrgCode!);
-            localStorage.setItem('materials_name', helper.isNullOrUndefinedOrEmptyString(current?.materialsName) ? '' : current?.materialsName!);
-            localStorage.setItem('materials_model', helper.isNullOrUndefinedOrEmptyString(current?.materialsModel) ? '' : current?.materialsModel!);
-            localStorage.setItem('materials_hardware_version', helper.isNullOrUndefinedOrEmptyString(current?.materialsHardwareVersion) ? '' : current?.materialsHardwareVersion!);
-            localStorage.setItem('materials_software_version', helper.isNullOrUndefinedOrEmptyString(current?.materialsSoftwareVersion) ? '' : current?.materialsSoftwareVersion!);
-            localStorage.setItem('materials_serial', helper.isNullOrUndefinedOrEmptyString(current?.materialsSerial) ? '' : current?.materialsSerial!);
-            localStorage.setItem('ip_address', helper.isNullOrUndefinedOrEmptyString(current?.ipAddress) ? '' : current?.ipAddress!);
-        } else {
-            message.error('保存失败');
-        }
-    };
+	useSubscribe('query-bcp-conf-result', queryBcpConfResultHandle);
+	useSubscribe('update-bcp-conf-result', updateBcpConfResultHandle);
+	useMount(() => {
+		ipcRenderer.send('query-bcp-conf');
+	});
 
-    useSubscribe('query-bcp-conf-result', queryBcpConfResultHandle);
-    useSubscribe('update-bcp-conf-result', updateBcpConfResultHandle);
-    useMount(() => {
-        ipcRenderer.send('query-bcp-conf');
-    });
+	const formSubmit = debounce(
+		() => {
+			const { validateFields } = props.form;
 
-    const formSubmit = debounce(() => {
-        const { validateFields } = props.form;
+			validateFields((err, values: FormValue) => {
+				if (!err) {
+					formValue.current = values;
+					ipcRenderer.send(
+						'update-bcp-conf',
+						values.manufacturer,
+						values.securitySoftwareOrgCode,
+						values.materialsName,
+						values.materialsModel,
+						values.materialsHardwareVersion,
+						values.materialsSoftwareVersion,
+						values.materialsSerial,
+						values.ipAddress,
+						values.id
+					);
+				}
+			});
+		},
+		500,
+		{ leading: true, trailing: false }
+	);
 
-        validateFields((err, values: FormValue) => {
-            if (!err) {
-                formValue.current = values;
-                ipcRenderer.send('update-bcp-conf',
-                    values.manufacturer,
-                    values.securitySoftwareOrgCode,
-                    values.materialsName,
-                    values.materialsModel,
-                    values.materialsHardwareVersion,
-                    values.materialsSoftwareVersion,
-                    values.materialsSerial,
-                    values.ipAddress,
-                    values.id
-                );
-            }
-        });
-    }, 500, { leading: true, trailing: false });
+	const formItemLayout = {
+		labelCol: { span: 4 },
+		wrapperCol: { span: 18 }
+	};
 
-    const formItemLayout = {
-        labelCol: { span: 4 },
-        wrapperCol: { span: 18 }
-    };
-
-    return <div className="bcp-conf-root">
-        <Title
-            onOk={() => {
-                formSubmit();
-            }}
-            okText="保存">
-            BCP生成信息配置</Title>
-        <div className="scroll-box">
-            <div className="form-box">
-                <Form {...formItemLayout}>
-                    <Item label="制造商名称">
-                        {
-                            getFieldDecorator('manufacturer', {
-                                initialValue: data.manufacturer
-                            })(<Input />)
-                        }
-                    </Item>
-                    <Item label="厂商组织机构代码">
-                        {
-                            getFieldDecorator('securitySoftwareOrgCode', {
-                                initialValue: data.security_software_orgcode
-                            })(<Input />)
-                        }
-                    </Item>
-                    <Item label="采集设备名称">
-                        {
-                            getFieldDecorator('materialsName', {
-                                initialValue: data.materials_name
-                            })(<Input />)
-                        }
-                    </Item>
-                    <Item label="设备型号">
-                        {
-                            getFieldDecorator('materialsModel', {
-                                initialValue: data.materials_model
-                            })(<Input />)
-                        }
-                    </Item>
-                    <Item label="设备硬件版本号">
-                        {
-                            getFieldDecorator('materialsHardwareVersion', {
-                                initialValue: data.materials_hardware_version
-                            })(<Input />)
-                        }
-                    </Item>
-                    <Item label="设备软件版本号">
-                        {
-                            getFieldDecorator('materialsSoftwareVersion', {
-                                initialValue: data.materials_software_version
-                            })(<Input />)
-                        }
-                    </Item>
-                    <Item label="设备序列号">
-                        {
-                            getFieldDecorator('materialsSerial', {
-                                initialValue: data.materials_serial
-                            })(<Input />)
-                        }
-                    </Item>
-                    <Item label="采集点IP">
-                        {
-                            getFieldDecorator('ipAddress', {
-                                initialValue: data.ip_address
-                            })(<Input />)
-                        }
-                    </Item>
-                    <Item style={{ display: 'none' }}>
-                        {
-                            getFieldDecorator('id', {
-                                initialValue: data.id
-                            })(<Input type="" />)
-                        }
-                    </Item>
-
-                </Form>
-            </div>
-        </div>
-    </div>
+	return (
+		<div className="bcp-conf-root">
+			<Title
+				onOk={() => {
+					formSubmit();
+				}}
+				okText="保存">
+				软硬件信息配置
+			</Title>
+			<div className="scroll-box">
+				<div className="form-box">
+					<Form {...formItemLayout}>
+						<Item label="制造商名称">
+							{getFieldDecorator('manufacturer', {
+								initialValue: data.manufacturer
+							})(<Input />)}
+						</Item>
+						<Item label="厂商组织机构代码">
+							{getFieldDecorator('securitySoftwareOrgCode', {
+								initialValue: data.security_software_orgcode
+							})(<Input />)}
+						</Item>
+						<Item label="采集设备名称">
+							{getFieldDecorator('materialsName', {
+								initialValue: data.materials_name
+							})(<Input />)}
+						</Item>
+						<Item label="设备型号">
+							{getFieldDecorator('materialsModel', {
+								initialValue: data.materials_model
+							})(<Input />)}
+						</Item>
+						<Item label="设备硬件版本号">
+							{getFieldDecorator('materialsHardwareVersion', {
+								initialValue: data.materials_hardware_version
+							})(<Input />)}
+						</Item>
+						<Item label="设备软件版本号">
+							{getFieldDecorator('materialsSoftwareVersion', {
+								initialValue: data.materials_software_version
+							})(<Input />)}
+						</Item>
+						<Item label="设备序列号">
+							{getFieldDecorator('materialsSerial', {
+								initialValue: data.materials_serial
+							})(<Input />)}
+						</Item>
+						<Item label="采集点IP">
+							{getFieldDecorator('ipAddress', {
+								initialValue: data.ip_address
+							})(<Input />)}
+						</Item>
+						<Item style={{ display: 'none' }}>
+							{getFieldDecorator('id', {
+								initialValue: data.id
+							})(<Input type="" />)}
+						</Item>
+					</Form>
+				</div>
+			</div>
+		</div>
+	);
 });
 
 export default BcpConf;
