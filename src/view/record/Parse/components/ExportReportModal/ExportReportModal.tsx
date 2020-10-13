4e7a7f513abed7ad30c42b3712e9ad1970c0d96b
cuiyue <cuiyue.msn@hotmail.com>
2020-10-13 19:03:36 +0800

    导出报告完成

--- src/view/record/Parse/components/ExportReportModal/ExportReportModal.tsx
+++ src/view/record/Parse/components/ExportReportModal/ExportReportModal.tsx
@@ -1,17 +1,17 @@
 import path from 'path';
-import React, { FC, useEffect, useState } from 'react';
+import { OpenDialogReturnValue, remote } from 'electron';
+import React, { FC, memo, useEffect, useState } from 'react';
 import $ from 'jquery';
-import cpy from 'cpy';
 import Button from 'antd/lib/button';
 import Checkbox from 'antd/lib/checkbox';
 import Modal from 'antd/lib/modal';
+import message from 'antd/lib/message';
 import { helper } from '@src/utils/helper';
 import { Prop } from './componentTypes';
+import { expandNodes, filterTree, mapTree, readTxtFile } from './treeUtil';
 import '@ztree/ztree_v3/js/jquery.ztree.all.min';
 import '@ztree/ztree_v3/css/zTreeStyle/zTreeStyle.css';
 import './ExportReportModal.less';
-import { expandNodes, filterTree, mapTree, readTxtFile } from './treeUtil';
-import { message } from 'antd';
 
 let ztree: any = null;
 
@@ -57,6 +57,75 @@ const ExportReportModal: FC<Prop> = (props) => {
 		}
 	}, [props.visible]);
 
+	/**
+	 * 选择导出目录
+	 */
+	const selectExportDir = () => {
+		remote.dialog
+			.showOpenDialog({
+				title: '请选择保存目录',
+				properties: ['openDirectory', 'createDirectory']
+			})
+			.then(async (val: OpenDialogReturnValue) => {
+				const { mobileHolder, mobileName } = props.device!;
+				const reportName = `${mobileHolder}-${mobileName?.split('_')[0]}分析报告`;
+				if (val.filePaths && val.filePaths.length > 0) {
+					const modal = Modal.info({
+						content: '正在导出报告... 可能时间较长，请等待',
+						okText: '确定',
+						maskClosable: false,
+						okButtonProps: { disabled: true, icon: 'loading' }
+					});
+
+					const [dir] = val.filePaths; //用户所选目标目录
+					const [tree, files] = filterTree(ztree.getNodes());
+					const reportRoot = path.join(props.device?.phonePath!, 'report'); //当前报告目录
+
+					try {
+						await Promise.all([
+							//若要在拷贝目录时保持层级结构，要使用parents:true,并使用cwd来指定从哪里查找
+							helper.copyFiles(
+								[
+									'assert/**/*',
+									'fonts/**/*',
+									'public/images/**/*',
+									'index.html',
+									'*.js'
+								],
+								path.join(dir, reportName),
+								{
+									parents: true,
+									cwd: reportRoot
+								}
+							),
+							helper.copyFiles(
+								files.map((f) => path.join(reportRoot, 'public/data', f)),
+								path.join(dir, reportName, 'public/data')
+							)
+						]);
+						await helper.writeJSONfile(
+							path.join(dir, reportName, 'public/data/tree.json'),
+							`;var data=${JSON.stringify(tree)}`
+						);
+						closeHandle();
+						modal.update({
+							content: '导出报告成功',
+							okButtonProps: { disabled: false, icon: 'check-circle' }
+						});
+						setTimeout(() => {
+							modal.destroy();
+						}, 600);
+					} catch (error) {
+						modal.update({
+							title: '导出失败',
+							content: error.message,
+							okButtonProps: { disabled: false, icon: 'check-circle' }
+						});
+					}
+				}
+			});
+	};
+
 	const closeHandle = () => {
 		props.closeHandle!();
 	};
@@ -76,60 +145,13 @@ const ExportReportModal: FC<Prop> = (props) => {
 					type="primary"
 					icon="export"
 					onClick={async () => {
-						let [tree, files] = filterTree(ztree.getNodes());
-						const reportRoot = path.join(props.device?.phonePath!, 'report');
+						let [, files] = filterTree(ztree.getNodes());
 						if (files.length === 0) {
-							message.info('请选择导出数据');
-							return;
+							message.destroy();
+							message.info('请选择报告数据');
+						} else {
+							selectExportDir();
 						}
-
-						await Promise.all([
-							helper.copyFiles(
-								[
-									path.join(reportRoot, 'index.html'),
-									path.join(reportRoot, '*.js')
-								],
-								'E:/target/report'
-							),
-							//若要在拷贝目录时保持层级结构，要使用parents:true,并使用cwd来指定从哪里查找
-							helper.copyFiles('assert/**/*', 'E:/target/report', {
-								parents: true,
-								cwd: reportRoot
-							}),
-							helper.copyFiles('fonts/**/*', 'E:/target/report', {
-								parents: true,
-								cwd: reportRoot
-							}),
-							helper.copyFiles('images/**/*', 'E:/target/report/public', {
-								parents: true,
-								cwd: path.join(reportRoot, 'public')
-							}),
-							helper.copyFiles(
-								files.map((f) => path.join(reportRoot, 'public/data', f)),
-								'E:/target/report/public/data'
-							)
-						]);
-						await helper.writeJSONfile(
-							'E:/target/report/public/data/tree.json',
-							`;var data=${JSON.stringify(tree)}`
-						),
-							// Promise.all([
-							// 	helper.writeJSONfile(
-							// 		'E:/target/tree.json',
-							// 		`;var data=${JSON.stringify(tree)}`
-							// 	),
-							// 	cpy(
-							// 		[
-							// 			'E:/TZTest/测试报告_20201013113743/长者/KNT-UL10_20201013113753/report/fonts/**/*'
-							// 		],
-							// 		'E:/target'
-							// 	)
-							// ])
-							// 	.then(() => console.log('拷贝完成'))
-							// 	.catch((err) => console.log(err));
-							console.clear();
-						console.log(files);
-						console.log('拷贝完成...');
 					}}>
 					导出
 				</Button>
@@ -154,4 +176,4 @@ ExportReportModal.defaultProps = {
 	closeHandle: () => {}
 };
 
-export default ExportReportModal;
+export default memo(ExportReportModal);
