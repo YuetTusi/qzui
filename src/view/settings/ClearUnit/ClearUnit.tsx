1cc552f8afe79e69b30e8c95e4c4d28c6f33b34f
cuiyue <cuiyue.msn@hotmail.com>
2020-10-17 16:00:05 +0800

    清空单位设置同步更新unit.json

--- src/view/settings/ClearUnit/ClearUnit.tsx
+++ src/view/settings/ClearUnit/ClearUnit.tsx
@@ -1,73 +1,103 @@
+import path from 'path';
+import { remote } from 'electron';
 import React, { FC, useState } from 'react';
 import Button from 'antd/lib/button';
 import { useMount } from '@src/hooks';
 import { LocalStoreKey } from '@src/utils/localStore';
 import './ClearUnit.less';
+import { helper } from '@src/utils/helper';
 
+let jsonSavePath = ''; //JSON文件路径
+if (process.env['NODE_ENV'] === 'development') {
+	jsonSavePath = path.join(remote.app.getAppPath(), './data/unit.json');
+} else {
+	jsonSavePath = path.join(remote.app.getAppPath(), '../data/unit.json');
+}
 
-interface Prop { };
+interface Prop {}
 
 /**
  * 清空单位数据
- * @param props 
+ * @param props
  */
 const ClearUnit: FC<Prop> = (props) => {
+	const [unitName, setUnitName] = useState<string | null>(null);
+	const [unitCode, setUnitCode] = useState<string | null>(null);
+	const [dstUnitName, setDstUnitName] = useState<string | null>(null);
+	const [dstUnitCode, setDstUnitCode] = useState<string | null>(null);
 
-    const [unitName, setUnitName] = useState<string | null>(null);
-    const [unitCode, setUnitCode] = useState<string | null>(null);
-    const [dstUnitName, setDstUnitName] = useState<string | null>(null);
-    const [dstUnitCode, setDstUnitCode] = useState<string | null>(null);
+	useMount(() => {
+		setUnitName(localStorage.getItem(LocalStoreKey.UnitName));
+		setUnitCode(localStorage.getItem(LocalStoreKey.UnitCode));
+		setDstUnitName(localStorage.getItem(LocalStoreKey.DstUnitName));
+		setDstUnitCode(localStorage.getItem(LocalStoreKey.DstUnitCode));
+	});
 
-    useMount(() => {
-        setUnitName(localStorage.getItem(LocalStoreKey.UnitName));
-        setUnitCode(localStorage.getItem(LocalStoreKey.UnitCode));
-        setDstUnitName(localStorage.getItem(LocalStoreKey.DstUnitName));
-        setDstUnitCode(localStorage.getItem(LocalStoreKey.DstUnitCode));
-    });
-
-    return <div className="clear-unit-root">
-        <div className="clear-unit-box">
-            <div className="button-box">
-                <Button type="danger" onClick={() => {
-                    localStorage.removeItem(LocalStoreKey.UnitName);
-                    localStorage.removeItem(LocalStoreKey.UnitCode);
-                    setUnitName(null);
-                    setUnitCode(null);
-                }}>清空</Button>
-            </div>
-            <div>
-                <div>
-                    <label>当前采集单位：</label>
-                    <em>{unitName === null ? '未设置' : unitName}</em>
-                </div>
-                <div>
-                    <label>当前采集单位编号：</label>
-                    <em>{unitCode === null ? '未设置' : unitCode}</em>
-                </div>
-            </div>
-        </div>
-        <hr />
-        <div className="clear-unit-box">
-            <div className="button-box">
-                <Button type="danger" onClick={() => {
-                    localStorage.removeItem(LocalStoreKey.DstUnitName);
-                    localStorage.removeItem(LocalStoreKey.DstUnitCode);
-                    setDstUnitName(null);
-                    setDstUnitCode(null);
-                }}>清空</Button>
-            </div>
-            <div>
-                <div>
-                    <label>当前目的检验单位：</label>
-                    <em>{dstUnitName === null ? '未设置' : dstUnitName}</em>
-                </div>
-                <div>
-                    <label>当前目的检验单位编号：</label>
-                    <em>{dstUnitCode === null ? '未设置' : dstUnitCode}</em>
-                </div>
-            </div>
-        </div>
-    </div>;
+	return (
+		<div className="clear-unit-root">
+			<div className="clear-unit-box">
+				<div className="button-box">
+					<Button
+						type="danger"
+						onClick={() => {
+							localStorage.removeItem(LocalStoreKey.UnitName);
+							localStorage.removeItem(LocalStoreKey.UnitCode);
+							setUnitName(null);
+							setUnitCode(null);
+							helper.writeJSONfile(jsonSavePath, {
+								unitName: null,
+								unitCode: null,
+								dstUnitName,
+								dstUnitCode
+							});
+						}}>
+						清空
+					</Button>
+				</div>
+				<div>
+					<div>
+						<label>当前采集单位：</label>
+						<em>{unitName === null ? '未设置' : unitName}</em>
+					</div>
+					<div>
+						<label>当前采集单位编号：</label>
+						<em>{unitCode === null ? '未设置' : unitCode}</em>
+					</div>
+				</div>
+			</div>
+			<hr />
+			<div className="clear-unit-box">
+				<div className="button-box">
+					<Button
+						type="danger"
+						onClick={() => {
+							localStorage.removeItem(LocalStoreKey.DstUnitName);
+							localStorage.removeItem(LocalStoreKey.DstUnitCode);
+							setDstUnitName(null);
+							setDstUnitCode(null);
+							helper.writeJSONfile(jsonSavePath, {
+								unitName,
+								unitCode,
+								dstUnitName: null,
+								dstUnitCode: null
+							});
+						}}>
+						清空
+					</Button>
+				</div>
+				<div>
+					<div>
+						<label>当前目的检验单位：</label>
+						<em>{dstUnitName === null ? '未设置' : dstUnitName}</em>
+					</div>
+					<div>
+						<label>当前目的检验单位编号：</label>
+						<em>{dstUnitCode === null ? '未设置' : dstUnitCode}</em>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ClearUnit;
