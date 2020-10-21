27897050694589ffedeef4125601fa6edc2b40d0
cuiyue <cuiyue.msn@hotmail.com>
2020-10-21 16:01:46 +0800

    http路由描述

--- api.js
+++ api.js
@@ -6,20 +6,23 @@ const { ipcMain } = require('electron');
  * @param webContents
  */
 function api(webContents) {
-    const router = express.Router();
+	const router = express.Router();
 
-    router.get('/', (req, res) => {
-        res.json({ data: 'rootPage' });
-    });
+	router.get('/', (req, res) => {
+		res.json({
+			data: 'HTTP接口',
+			routes: [{ path: '/case', desc: '案件数据（解析完成&解析异常）' }]
+		});
+	});
 
-    router.get('/case', (req, res) => {
-        ipcMain.once('query-case-result', (event, result) => {
-            res.json(result);
-        });
-        webContents.send('query-case');
-    });
+	router.get('/case', (req, res) => {
+		ipcMain.once('query-case-result', (event, result) => {
+			res.json(result);
+		});
+		webContents.send('query-case');
+	});
 
-    return router;
+	return router;
 }
 
 module.exports = api;
