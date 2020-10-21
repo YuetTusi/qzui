bd063491ff5c74d5fa3483e6a49c226edd33cdb6
cuiyue <cuiyue.msn@hotmail.com>
2020-10-21 19:18:32 +0800

    军队版本隐藏案件BCP相关列

--- src/view/record/Parse/columns.tsx
+++ src/view/record/Parse/columns.tsx
@@ -5,103 +5,116 @@ import Tag from 'antd/lib/tag';
 import Modal from 'antd/lib/modal';
 import { Dispatch } from 'redux';
 import { ColumnGroupProps } from 'antd/lib/table/ColumnGroup';
+import { UseMode } from '@src/schema/UseMode';
 import DeviceType from '@src/schema/socket/DeviceType';
 import CCaseInfo from '@src/schema/CCaseInfo';
+import { helper } from '@src/utils/helper';
+
+const config = helper.readConf();
 
 /**
  * 表头定义
  * @param dispatch 派发方法
  */
 export function getColumns<T>(dispatch: Dispatch<T>): ColumnGroupProps[] {
-    const columns = [
-        {
-            title: '案件名称',
-            dataIndex: 'm_strCaseName',
-            key: 'm_strCaseName',
-            render: (cell: string) => {
-                return cell.includes('_') ? cell.split('_')[0] : cell;
-            }
-        },
-        {
-            title: '拉取SD卡',
-            dataIndex: 'sdCard',
-            key: 'sdCard',
-            width: '100px',
-            align: 'center',
-            render: (val: boolean) =>
-                val ? <Tag color="green">是</Tag> : <Tag color="red">否</Tag>
-        },
-        {
-            title: '自动解析',
-            dataIndex: 'm_bIsAutoParse',
-            key: 'm_bIsAutoParse',
-            width: '100px',
-            align: 'center',
-            render: (val: boolean) =>
-                val ? <Tag color="green">是</Tag> : <Tag color="red">否</Tag>
-        },
-        {
-            title: '生成BCP',
-            dataIndex: 'generateBcp',
-            key: 'generateBcp',
-            width: '100px',
-            align: 'center',
-            render: (val: boolean) =>
-                val ? <Tag color="green">是</Tag> : <Tag color="red">否</Tag>
-        },
-        {
-            title: '包含附件',
-            dataIndex: 'attachment',
-            key: 'attachment',
-            width: '100px',
-            align: 'center',
-            render: (val: boolean) =>
-                val ? <Tag color="green">是</Tag> : <Tag color="red">否</Tag>
-        },
-        {
-            title: '创建时间',
-            dataIndex: 'cTime',
-            key: 'cTime',
-            width: '200px',
-            align: 'center',
-            sorter: (m: DeviceType, n: DeviceType) =>
-                moment(m.createdAt).isAfter(moment(n.createdAt)) ? 1 : -1,
-            render: (val: any, record: DeviceType) =>
-                moment(record.createdAt).format('YYYY年M月D日 HH:mm:ss')
-        },
-        {
-            title: '删除',
-            dataIndex: '_id',
-            key: 'del',
-            width: '100px',
-            align: 'center',
-            render: (id: string, record: CCaseInfo) => (
-                <a
-                    onClick={(e: MouseEvent<HTMLAnchorElement>) => {
-                        e.stopPropagation();
-                        const casePath = path.join(record.m_strCasePath, record.m_strCaseName);
-                        const [caseName] = record.m_strCaseName.split('_');
-                        Modal.confirm({
-                            title: `删除「${caseName}」`,
-                            content: `请确认所有设备解析完成`,
-                            okText: '是',
-                            cancelText: '否',
-                            onOk() {
-                                dispatch({
-                                    type: 'parse/deleteCaseData',
-                                    payload: {
-                                        id: record._id,
-                                        casePath
-                                    }
-                                });
-                            }
-                        });
-                    }}
-                >
-                    删除
-                </a>
-            )
-        }
-    ];
-    return columns;
+	const columns = [
+		{
+			title: '案件名称',
+			dataIndex: 'm_strCaseName',
+			key: 'm_strCaseName',
+			render: (cell: string) => {
+				return cell.includes('_') ? cell.split('_')[0] : cell;
+			}
+		},
+		{
+			title: '拉取SD卡',
+			dataIndex: 'sdCard',
+			key: 'sdCard',
+			width: '100px',
+			align: 'center',
+			render: (val: boolean) =>
+				val ? <Tag color="green">是</Tag> : <Tag color="red">否</Tag>
+		},
+		{
+			title: '自动解析',
+			dataIndex: 'm_bIsAutoParse',
+			key: 'm_bIsAutoParse',
+			width: '100px',
+			align: 'center',
+			render: (val: boolean) =>
+				val ? <Tag color="green">是</Tag> : <Tag color="red">否</Tag>
+		},
+		{
+			title: '生成BCP',
+			dataIndex: 'generateBcp',
+			key: 'generateBcp',
+			width: '100px',
+			align: 'center',
+			render: (val: boolean) =>
+				val ? <Tag color="green">是</Tag> : <Tag color="red">否</Tag>
+		},
+		{
+			title: '包含附件',
+			dataIndex: 'attachment',
+			key: 'attachment',
+			width: '100px',
+			align: 'center',
+			render: (val: boolean) =>
+				val ? <Tag color="green">是</Tag> : <Tag color="red">否</Tag>
+		},
+		{
+			title: '创建时间',
+			dataIndex: 'cTime',
+			key: 'cTime',
+			width: '200px',
+			align: 'center',
+			sorter: (m: DeviceType, n: DeviceType) =>
+				moment(m.createdAt).isAfter(moment(n.createdAt)) ? 1 : -1,
+			render: (val: any, record: DeviceType) =>
+				moment(record.createdAt).format('YYYY年M月D日 HH:mm:ss')
+		},
+		{
+			title: '删除',
+			dataIndex: '_id',
+			key: 'del',
+			width: '100px',
+			align: 'center',
+			render: (id: string, record: CCaseInfo) => (
+				<a
+					onClick={(e: MouseEvent<HTMLAnchorElement>) => {
+						e.stopPropagation();
+						const casePath = path.join(record.m_strCasePath, record.m_strCaseName);
+						const [caseName] = record.m_strCaseName.split('_');
+						Modal.confirm({
+							title: `删除「${caseName}」`,
+							content: `请确认所有设备解析完成`,
+							okText: '是',
+							cancelText: '否',
+							onOk() {
+								dispatch({
+									type: 'parse/deleteCaseData',
+									payload: {
+										id: record._id,
+										casePath
+									}
+								});
+							}
+						});
+					}}>
+					删除
+				</a>
+			)
+		}
+	];
+
+	console.log(config.useMode);
+
+	if (config.useMode === UseMode.Army) {
+		//?军队版隐藏BCP和附件列
+		return columns.filter(
+			(item: any) => !(item.title === '生成BCP' || item.title === '包含附件')
+		);
+	} else {
+		return columns;
+	}
 }
