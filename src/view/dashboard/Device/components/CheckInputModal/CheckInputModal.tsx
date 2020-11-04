73e81e0638a4a265a5e3d02b1f4f8fd4098b280a
cuiyue <cuiyue.msn@hotmail.com>
2020-11-04 15:43:43 +0800

    点验输入框绑定是否生成报告

--- src/view/dashboard/Device/components/CheckInputModal/CheckInputModal.tsx
+++ src/view/dashboard/Device/components/CheckInputModal/CheckInputModal.tsx
@@ -28,6 +28,7 @@ const CheckInputModal: FC<Prop> = (props) => {
 	const casePath = useRef<string>(''); //案件存储路径
 	const appList = useRef<any[]>([]); //解析App
 	const sdCard = useRef<boolean>(false); //是否拉取SD卡
+	const hasReport = useRef<boolean>(false); //是否生成报告
 	const isAuto = useRef<boolean>(false); //是否自动解析
 	const unitName = useRef<string>(''); //检验单位
 
@@ -52,6 +53,7 @@ const CheckInputModal: FC<Prop> = (props) => {
 					data-case-path={opt.m_strCasePath}
 					data-app-list={opt.m_Applist}
 					data-sdcard={opt.sdCard}
+					data-has-report={opt.hasReport}
 					data-is-auto={opt.m_bIsAutoParse}
 					data-unitname={opt.m_strCheckUnitName}
 					key={opt._id}>
@@ -71,6 +73,7 @@ const CheckInputModal: FC<Prop> = (props) => {
 		casePath.current = (option as JSX.Element).props['data-case-path'] as string;
 		appList.current = (option as JSX.Element).props['data-app-list'] as any[];
 		sdCard.current = (option as JSX.Element).props['data-sdcard'] as boolean;
+		hasReport.current = (option as JSX.Element).props['data-has-report'] as boolean;
 		isAuto.current = (option as JSX.Element).props['data-is-auto'] as boolean;
 		unitName.current = (option as JSX.Element).props['data-unitname'] as string;
 	}, []);
@@ -80,6 +83,7 @@ const CheckInputModal: FC<Prop> = (props) => {
 		casePath.current = ''; //案件存储路径
 		appList.current = []; //解析App
 		sdCard.current = false; //拉取SD卡
+		hasReport.current = false; //是否生成报告
 		isAuto.current = false; //是否自动解析
 		unitName.current = ''; //检验单位
 	}, []);
@@ -100,6 +104,7 @@ const CheckInputModal: FC<Prop> = (props) => {
 				entity.caseId = caseId.current;
 				entity.casePath = casePath.current;
 				entity.sdCard = sdCard.current ?? false;
+				entity.hasReport = hasReport.current ?? false;
 				entity.isAuto = isAuto.current;
 				entity.unitName = unitName.current;
 				entity.mobileName = `${values.phoneName}_${helper.timestamp(device?.usb)}`;
