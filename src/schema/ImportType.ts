1ab0084086cdff489f95da1e65f0fc0206b429e1
cuiyue <cuiyue.msn@hotmail.com>
2021-01-29 14:59:38 +0800

    增加oppo和vivo第三方导入类型

--- src/schema/ImportType.ts
+++ src/schema/ImportType.ts
@@ -3,5 +3,7 @@
  */
 export const importTypes = [
     { name: '华为Hisuite备份', value: 'hisuite' },
-    { name: '苹果iTunes备份', value: 'ios' }
+    { name: '苹果iTunes备份', value: 'ios' },
+    { name: 'VIVO自备份', value: 'vivo_easyshare' },
+    { name: 'OPPO自备份', value: 'oppo_backup' },
 ]
