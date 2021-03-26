1e236715989495037bd16f392420cf9a0b15d92d
cuiyue <cuiyue.msn@hotmail.com>
2021-03-26 13:02:02 +0800

    增加解析应用

--- src/schema/ImportType.ts
+++ src/schema/ImportType.ts
@@ -1,14 +1,4 @@
 /**
- * 第三方导入数据类型
- */
-export const importTypes = [
-    { name: '华为Hisuite备份', value: 'hisuite' },
-    { name: '苹果iTunes备份', value: 'ios' },
-    { name: 'VIVO自备份', value: 'vivo_easyshare' },
-    { name: 'OPPO自备份', value: 'oppo_backup' },
-]
-
-/**
  * 导入方式枚举
  */
 export enum ImportTypes {
