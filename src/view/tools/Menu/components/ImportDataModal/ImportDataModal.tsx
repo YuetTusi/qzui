f3e8570058902d541effb16c46c0a4b6a7bb69a0
cuiyue <cuiyue.msn@hotmail.com>
2021-03-26 13:04:50 +0800

    第三方导入加防抖处理

--- src/view/tools/Menu/components/ImportDataModal/ImportDataModal.tsx
+++ src/view/tools/Menu/components/ImportDataModal/ImportDataModal.tsx
@@ -1,6 +1,7 @@
 import path from 'path';
 import { remote } from 'electron';
 import React, { FC, MouseEvent, useRef } from 'react';
+import debounce from 'lodash/debounce';
 import { connect } from 'dva';
 import Modal from 'antd/lib/modal';
 import Button from 'antd/lib/button';
@@ -39,47 +40,51 @@ const ImportDataModal: FC<Prop> = (props) => {
 	 * @param fetchData 导入的数据
 	 * @param packagePath 第三方数据路径
 	 */
-	const saveDeviceToCase = async (fetchData: FetchData, packagePath: string) => {
-		const { dispatch, type } = props;
-		const db: DbInstance<CCaseInfo> = getDb(TableName.Case);
-		try {
-			let caseData = await db.findOne({ _id: fetchData.caseId });
+	const saveDeviceToCase = debounce(
+		async (fetchData: FetchData, packagePath: string) => {
+			const { dispatch, type } = props;
+			const db: DbInstance<CCaseInfo> = getDb(TableName.Case);
+			try {
+				let caseData = await db.findOne({ _id: fetchData.caseId });
 
-			if (caseData === null) {
-				message.destroy();
-				message.error('查无此案件，请重新选择');
-			} else {
-				let rec = new DeviceType();
-				rec.id = helper.newId();
-				rec.mobileHolder = fetchData.mobileHolder;
-				rec.mobileNo = fetchData.mobileNo;
-				rec.mobileName = `${fetchData.mobileName}_${helper.timestamp()}`;
-				rec.note = fetchData.note;
-				rec.fetchTime = new Date();
-				rec.phonePath = path.join(
-					caseData.m_strCasePath,
-					caseData.m_strCaseName!,
-					fetchData.mobileHolder!,
-					rec.mobileName
-				);
-				rec.fetchState = FetchState.Finished;
-				rec.mode = DataMode.Self;
-				rec.caseId = fetchData.caseId; //所属案件id
-				rec.parseState = ParseState.Parsing;
-				rec.system = type === 'ios' ? PhoneSystem.IOS : PhoneSystem.Android;
+				if (caseData === null) {
+					message.destroy();
+					message.error('查无此案件，请重新选择');
+				} else {
+					let rec = new DeviceType();
+					rec.id = helper.newId();
+					rec.mobileHolder = fetchData.mobileHolder;
+					rec.mobileNo = fetchData.mobileNo;
+					rec.mobileName = `${fetchData.mobileName}_${helper.timestamp()}`;
+					rec.note = fetchData.note;
+					rec.fetchTime = new Date();
+					rec.phonePath = path.join(
+						caseData.m_strCasePath,
+						caseData.m_strCaseName!,
+						fetchData.mobileHolder!,
+						rec.mobileName
+					);
+					rec.fetchState = FetchState.Finished;
+					rec.mode = DataMode.Self;
+					rec.caseId = fetchData.caseId; //所属案件id
+					rec.parseState = ParseState.Parsing;
+					rec.system = type === 'ios' ? PhoneSystem.IOS : PhoneSystem.Android;
 
-				//NOTE:将设备数据入库并通知Parse开始导入
-				dispatch({
-					type: 'importDataModal/saveImportDeviceToCase',
-					payload: { device: rec, packagePath, dataType: type }
-				});
-				props.cancelHandle!();
-				message.info('正在导入...请在「数据解析」页查看解析进度');
+					//NOTE:将设备数据入库并通知Parse开始导入
+					dispatch({
+						type: 'importDataModal/saveImportDeviceToCase',
+						payload: { device: rec, packagePath, dataType: type }
+					});
+					props.cancelHandle!();
+					message.info('正在导入...请在「数据解析」页查看解析进度');
+				}
+			} catch (error) {
+				console.log(error);
 			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+		},
+		400,
+		{ leading: true, trailing: false }
+	);
 
 	/**
 	 * 表单提交
