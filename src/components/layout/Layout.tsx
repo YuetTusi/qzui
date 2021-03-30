a4527ef387192a91f7b2d836304c4e2d4bbfd6e4
cuiyue <cuiyue.msn@hotmail.com>
2021-03-30 14:08:50 +0800

    调整样式

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,16 +7,13 @@ interface Prop {}
 
 /**
  * @description 布局组件
- * @param props
  */
-const Layout: FC<Prop> = (props) => {
-	return (
-		<>
-			<Nav />
-			<div className="bottom-root">{props.children}</div>
-			<AlarmMessage />
-		</>
-	);
-};
+const Layout: FC<Prop> = (props) => (
+	<>
+		<Nav />
+		<div className="bottom-root">{props.children}</div>
+		<AlarmMessage />
+	</>
+);
 
 export default Layout;
