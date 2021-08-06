394fb48be8a04fab1813c817660b665181c3926b
cuiyue <cuiyue.msn@hotmail.com>
2021-08-06 10:53:50 +0800

    生成Bcp.json加remark字段

--- src/view/record/Bcp/Bcp.tsx
+++ src/view/record/Bcp/Bcp.tsx
@@ -286,7 +286,7 @@ const Bcp = Form.create<Prop>({ name: 'bcpForm' })((props: Prop) => {
 				} else {
 					const bcp = new BcpEntity();
 					bcp.mobilePath = deviceData?.phonePath! ?? '';
-					bcp.remark=deviceData?.note ?? '';
+					bcp.remark = deviceData?.note ?? '';
 					bcp.attachment = values.attachment;
 					bcp.checkUnitName = caseData.m_strCheckUnitName ?? '';
 					bcp.unitNo = values.unit ?? '';
