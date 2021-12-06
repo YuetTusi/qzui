c51b563a642230980cc96105f5135610deaf6077
cuiyue <cuiyue.msn@hotmail.com>
2021-12-06 15:47:18 +0800

    日志输出全部内容

--- src/service/tcpServer.ts
+++ src/service/tcpServer.ts
@@ -104,7 +104,7 @@ function send(type: string, data: Record<string, any>) {
         current.socket.write(head);
         current.socket.write(body);
         logger.info(
-            `发送消息 type:${type}, port:${current.port}, data:${JSON.stringify({ cmd: data?.cmd, msg: data?.msg })}`);
+            `发送消息 type:${type}, port:${current.port}, data:${JSON.stringify(data)}`);
     } else {
         console.warn(`${type} socket为空`);
     }
