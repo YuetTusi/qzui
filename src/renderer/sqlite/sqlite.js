b1e93802837877e1b363f452040f039e0cd16f31
cuiyue <cuiyue.msn@hotmail.com>
2022-06-07 11:24:31 +0800

    单位编号正序

--- src/renderer/sqlite/sqlite.js
+++ src/renderer/sqlite/sqlite.js
@@ -49,7 +49,7 @@ function queryUnit(keyword, current = 1, pageSize = 10) {
 		totalSqlParams.push(`%${keyword}%`);
 	}
 
-	pageSql += ' order by [PcsID] desc';
+	pageSql += ' order by [PcsID] asc';
 
 	pageSql += ' limit ? offset ? ';
 	pageSqlParams.push(pageSize);
