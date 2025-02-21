ef122b50d9846e0ee8c990a8b0ba0c2d82f71e79
cuiyue <cuiyue.msn@hotmail.com>
2025-02-21 10:59:30 +0800

    更新文档

--- src/components/ErrorBoundary/ErrorMessage.tsx
+++ src/components/ErrorBoundary/ErrorMessage.tsx
@@ -7,18 +7,14 @@ interface Prop {
 	title: string;
 }
 
-const ErrorMessage: FC<Prop> = (props) => {
-	return (
-		<div className="error-message-root">
-			<div className="warn-bg">
-				<div className="err-caption">{props.title ?? '取证程序暂时有些问题'}</div>
-				<div className="err-message">
-					<FontAwesomeIcon className="warn-icon" icon={faExclamationTriangle} />
-					<div className="err-info-box">{props.children}</div>
-				</div>
-			</div>
+const ErrorMessage: FC<Prop> = ({ children, title }) => <div className="error-message-root">
+	<div className="warn-bg">
+		<div className="err-caption">{title ?? '取证程序暂时有些问题'}</div>
+		<div className="err-message">
+			<FontAwesomeIcon className="warn-icon" icon={faExclamationTriangle} />
+			<div className="err-info-box">{children}</div>
 		</div>
-	);
-};
+	</div>
+</div>;
 
 export { ErrorMessage };
